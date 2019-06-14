a2ad7c35b216e0c3b537d9125439b56cb8eaea35
jeffLasa <jeff.lasa@hotmail>
2019-06-13 21:33:19 -0300

    Fragments

---
+++ FragmentApp/app/src/main/java/com/example/fragmentapp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.fragmentapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        PrimeiroFragment primeiroFragment = new PrimeiroFragment();
+        transaction.replace(R.id.container_id,primeiroFragment);
+
+        SegundoFragment segundoFragment = new SegundoFragment();
+        transaction.replace(R.id.outro_container_id,segundoFragment);
+
+        transaction.commit();
+
+        Button primeiroButton = findViewById(R.id.primeiro_fragment_button);
+        primeiroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exibirPrimeiroFragment();
+            }
+        });
+
+        Button segundoButton = findViewById(R.id.segundo_fragment_button);
+        segundoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exibirSegundoFragment();
+            }
+        });
+
+
+    }
+
+    private void exibirPrimeiroFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container_id, new PrimeiroFragment());
+        transaction.replace(R.id.outro_container_id, new PrimeiroFragment());
+        transaction.commit();
+    }
+
+    private void exibirSegundoFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container_id, new SegundoFragment());
+        transaction.replace(R.id.outro_container_id, new SegundoFragment());
+        transaction.commit();
+    }
+
+
+
+}
+
+
